a8b1154e608c7da084bb87a951a928b4e113e41e
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-04-09 19:51:26 -0400

    HTML set up

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var app = express();
 var db = require('./server/index.js');
 var Salesperson = db.Salesperson;
 var routes = require('./server/routes/index.js');
+var path = require('path');
 
+app.use('/static', express.static(path.join(__dirname, '/public')));
 app.use(routes);
+
+
 
 var PORT = process.env.PORT||1337;
 app.listen(PORT, function(){
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+var indexHtml = path.join(__dirname, '..', '..', 'public', 'index.html');
 
 router.get('/', function(req,res){
-	res.send('Hello');
+	res.sendFile(indexHtml);
 });
 
 router.get('/printme/:printString', function(req,res){
