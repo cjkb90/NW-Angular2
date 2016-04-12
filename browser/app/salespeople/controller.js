350e88ca5e8bd8e9a8d18e4c8c610b8170cd3ead
Eric P. Katz <ericpkatz@gmail.com>
2016-04-12 09:58:23 -0400

    simplify code-- pass data back in repeater

--- browser/app/salespeople/controller.js
+++ browser/app/salespeople/controller.js
@@ -9,42 +9,47 @@ app.controller('SPCtrl', function($scope, $http, SPFactory, $log){
 			$scope.north = $scope.south = $scope.east = $scope.west = undefined;
 		});
 		getSalespeople();
-	}
+	};
 
 	$scope.regionsSelected = function(){
 		var regions = [];
-		if ($scope.north){regions.push('north')};
-		if ($scope.south){regions.push('south')};
-		if ($scope.east){regions.push('east')};
-		if ($scope.west){regions.push('west')};
+		if ($scope.north){
+      regions.push('north');
+    };
+		if ($scope.south){
+      regions.push('south');
+    };
+		if ($scope.east){
+      regions.push('east');
+    };
+		if ($scope.west){
+      regions.push('west');
+    };
 		return regions;
-	}
+	};
 
 	var getSalespeople = function(){
 		SPFactory.getAll()
 		.then(function(salespeople){
 			$scope.salespeople = salespeople;
 		});
-	}
+	};
+
 	getSalespeople();
 
-	$scope.delete = function(){
-		SPFactory.deleteById(this.salesperson._id)
+	$scope.delete = function(salesperson){
+		SPFactory.deleteById(salesperson._id);
 		getSalespeople();
-	}
-	$scope.regionToggle = function(region){
-		if (this.salesperson){
-			SPFactory.toggleRegion(this.salesperson, region);
+	};
+
+	$scope.regionToggle = function(region, salesperson){
+		if (salesperson){
+			SPFactory.toggleRegion(salesperson, region);
+      console.log(salesperson);
+      return;
 		}
 		else {
-			if (region == 'north') {$scope.north = !$scope.north}
-			if (region == 'south') {$scope.south = !$scope.south}
-			if (region == 'east') {$scope.east = !$scope.east}
-			if (region == 'west') {$scope.west = !$scope.west}
+			$scope[region] = !$scope[region];
 		}
-		getSalespeople();
-		$log.log(this);
-	}
-
-
+	};
 });
--- browser/app/salespeople/factory.js
+++ browser/app/salespeople/factory.js
@@ -7,38 +7,25 @@ app.factory('SPFactory', function($http, $log){
 		return $http.get('/api/salespeople')
 		.then(function(response){
 			return(response.data);
-		})
-	}
+		});
+	};
 
 	SPFactory.addSP = function(name, regions){
 		return $http.post('/api/salespeople/add/'+name, regions);
-	}
+	};
 
 	SPFactory.deleteById = function(id){
 		$http.delete('/api/salespeople/delete/'+id)
-	}
+	};
 
 	SPFactory.toggleRegion = function(salesperson, region){
-		$http.get('/api/salespeople/find/'+salesperson._id)
-		.then(function(response){
-			return(response.data);
-		})
-		.then(function(data){
-			$log.log(data.regions)
-			var index = data.regions.indexOf(region);
-			//This function is not working as intended. These changes aren't being stored for some reason
-			//if the region clicked is not part of the person's regions, then push it to the regions array
-			if(index<0){
-				data.regions.push(region);
-				$log.log(data.regions)
-			}
-			//if it is, then remove it from the array
-			else{
-				data.regions.splice(index, 1);
-			}
-			console.log(data)
-			$http.put('/api/salespeople/find/'+salesperson._id, data);
-		});
+    if(salesperson.regions.indexOf(region) === -1)
+      salesperson.regions.push(region);
+    else
+      salesperson.regions.splice(salesperson.regions.indexOf(region), 1);
+
+    //TODO - update the salesperson on the server
+			//$http.put('/api/salespeople/find/'+salesperson._id, data);
 	}
 
 	return SPFactory;
